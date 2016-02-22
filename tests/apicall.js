3dc1c9acd7b262bbbe9598de6a482d78bfb8a2ef
Albert Siddhartha Slawinski <errendir@gmail.com>
2016-02-22 16:43:36 +0100

    Add tests for the voting

---
+++ tests/apicall.js
@@ -0,0 +1,22 @@
+var sessionid = undefined;
+
+const fetch = require('node-fetch')
+
+module.exports.apicall = (path,data,method) => {
+  const headers = new fetch.Headers();
+  headers.append('Content-Type', 'application/json');
+  if(!!sessionid) {
+    headers.append('Cookie', 'sessionid=' + sessionid );
+  }
+
+  return fetch('http://localhost:3003/api/v1/' + path,{
+    method,
+    credentials: 'include',
+    body: method=='POST' ? JSON.stringify(data) : undefined,
+    headers
+  })
+  .then((response) => {
+    sessionid = sessionid || response.headers.get('set-cookie').split(';')[0].split('=')[1];
+    return response.json()
+  })
+}
